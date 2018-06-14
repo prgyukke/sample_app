4079a8b40262995358c72f56fec81ee00430abb7
Yuhsuke Gozu <yukkenottuna@gmail.com>
2018-06-14 10:48:46 +0900

    Userモデルにパスワードを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,9 @@ class User < ApplicationRecord
             length:     { maximum: 50 },
             format:     { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
+  validates :password,
+            presence: true,
+            length:   { minimum: 6 }
+
+  has_secure_password
 end
